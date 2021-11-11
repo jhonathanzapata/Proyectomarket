e8e26146d1dc0725fbee722a8de3ea1dba6e2ed8
ThaliZam <90801055+ThaliZam@users.noreply.github.com>
2021-11-11 11:40:38 -0500

    Revert "Formulario inicio de sesion y conexion a firebase"
    This reverts commit d2150475

--- app/src/main/java/com/example/proyectociclo4/MainActivity.kt
+++ app/src/main/java/com/example/proyectociclo4/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.proyectociclo4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AuthActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_main)
     }
 }
